65d434bf1bfc581f041afa92245d45d3e3571573
Okraks <antwisaac7@gmail.com>
2023-02-06 19:31:04 +0000

    perks

--- src/app/Perks/Perks.js
+++ src/app/Perks/Perks.js
@@ -9,19 +9,19 @@ import "./index.css";
 function Perks() {
   return (
     <Layout>
-      <div className="flex justify-between my-5 items-center mb-10">
-        <div className="flex-1">
-          <h2 className="text-primary text-6xl font-bold">
+      <div className="flex flex-col-reverse md:flex-row justify-between my-5 items-center mb-10">
+        <div className="flex-1 mt-4 md:mt-0">
+          <h2 className="text-primary text-4xl md:text-6xl font-bold">
             Signing up with <br /> Handys has its benefits
           </h2>
-          <p className="mt-4 font-light text-gray text-2xl">
+          <p className="mt-4 font-light text-gray text-lg md:text-2xl">
             Unlock valuable exclusive offers for your business with a
             significant potential benefit
           </p>
         </div>
         {/* image */}
         <div className="flex-1">
-          <img className="w-3/5 mx-auto" src={gifts} alt="gifts" />
+          <img className="w-4/5 md:w-3/5 mx-auto" src={gifts} alt="gifts" />
         </div>
       </div>
 
