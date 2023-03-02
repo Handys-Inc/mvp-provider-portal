21d37288a99e53eea4152506b9751886bd99ac83
Okraks <antwisaac7@gmail.com>
2023-03-02 13:46:24 +0000

    perks page fix

--- src/app/Perks/Perks.js
+++ src/app/Perks/Perks.js
@@ -4,7 +4,7 @@ import Layout from "../../components/Layout/Layout";
 import { MdArrowBack } from "react-icons/md";
 import { NavLink } from "react-router-dom";
 
-import Border from "../../components/Border/Border";
+
 
 import gifts from "./assets/gifts.svg";
 import Offers from "./components/Offers";
@@ -23,27 +23,31 @@ function Perks() {
           Perks
         </p>
       </div>
-      <div className="flex flex-col md:flex-row justify-between my-5 items-center pt-10 md:mt-0 mb-10">
-        <div className="flex-1 mt-4 md:mt-0">
-          <h2 className="hidden md:block text-primary text-4xl text-center md:text-left md:text-6xl font-bold">
-            Signing up with <br /> Handys has its benefits
-          </h2>
-          <h2 className="block md:hidden text-primary text-3xl text-center font-bold">
-            Signing up with Handys has its benefits
-          </h2>
-          <p className="mt-4 text-center md:text-left font-light text-gray text-lg md:text-2xl">
-            Unlock valuable exclusive offers for your business with a
-            significant potential benefit
-          </p>
+
+      <div className="max-w-7xl md:w-full mx-auto">
+        <div className="flex flex-col md:flex-row justify-between py-14 items-center pt-10 md:mt-0 mb-10">
+          <div className="flex-1 mt-4 md:mt-0">
+            <h2 className="hidden leading-10 md:block text-primary text-4xl text-center md:text-left md:text-6xl font-bold">
+              Signing up with <br /> Handys has its benefits
+            </h2>
+            <h2 className="block md:hidden text-primary text-3xl text-center font-bold">
+              Signing up with Handys has its benefits
+            </h2>
+            <p className="mt-10 w-full md:w-4/5 leading-10 text-center md:text-left font-light text-gray text-lg md:text-2xl">
+              Unlock valuable exclusive offers for your business with a
+              significant potential benefit
+            </p>
+          </div>
+          {/* image */}
+          <div className="flex-1">
+            <img className="w-4/5 md:w-3/5 mx-auto" src={gifts} alt="gifts" />
+          </div>
         </div>
-        {/* image */}
-        <div className="flex-1">
-          <img className="w-4/5 md:w-3/5 mx-auto" src={gifts} alt="gifts" />
-        </div>
+
+        {/* Offers */}
+        <Offers />
       </div>
 
-      {/* Offers */}
-      <Offers />
     </Layout>
   );
 }
